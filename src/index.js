146774b5049a064ce290799cdecd08d37b0dd955
Karol Sitarz <ka.sitarz@gmail.com>
2018-10-11 17:20:15 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import './styles/global-styles';
+
+const Index = () => {
+  return <div>dupa</div>;
+};
+
+ReactDOM.render(<Index />, document.getElementById("index"));
---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = (env,argv) => [
+  {
+    name: 'client',
+    target: 'web',
+    entry: './src/index.js',
+    module: {
+      rules: [{
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }]
+    },
+    devServer: {
+      contentBase: './dist',
+      hot: true,
+      open: true,
+      host: "127.0.0.1",
+      port: "1234"
+    },
+    plugins: [
+      argv.mode === 'production' ? undefined : new webpack.NamedModulesPlugin(),
+      argv.mode === 'production' ? undefined : new webpack.HotModuleReplacementPlugin(),
+      new HtmlWebPackPlugin({
+        template: "./src/index.html",
+        filename: "./index.html"
+      })
+    ].filter(Boolean),
+    node: {
+      __dirname: false
+    },
+    mode: "production",
+    performance: {
+      hints: argv.mode === 'production' ? "warning" : false
+    },
+    devtool: argv.mode === 'production' ? "none" : "inline-source-map"
+  },
+  {
+    name: 'server',
+    target: 'node',
+    entry: [
+      './src/server.js'
+    ],
+    mode: "production",
+    node: {
+      __dirname: false
+    },
+    devtool: argv.mode === 'production' ? "none" : "inline-source-map",
+    output: {
+      path: path.join(__dirname, '/dist'),
+      filename: 'server.js',
+      publicPath: '/'
+    }
+  }
+]
